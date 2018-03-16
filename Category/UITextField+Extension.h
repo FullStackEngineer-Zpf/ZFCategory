da62ee19797eee57915fbd59ce4524d36ad75784
mac <mac@macdeMacBook-Pro-2.local>
2018-03-16 18:13:30 +0800

    0.0.1

---
+++ Category/UITextField+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UITextField+Extension.h
+//  ShareMoney
+//
+//  Created by mac on 2017/7/27.
+//  Copyright © 2017年 Exmart_zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (Extension)
++(UITextField *)CreatTextFieldPlaceholder:(NSString *)placeholder;
+@end
